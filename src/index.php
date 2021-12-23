a16992ffd51c4958918eb482d511975be1c564c9
Andrew Elliott <aelliott@onatlas.com>
2021-12-22 16:26:06 -0800

    update pattern matching - remove named groups, match all word chars for file name

--- src/index.php
+++ src/index.php
@@ -1,14 +1,15 @@
 <?php
 //echo 'Hello world from my own Mac OS X server';
 $req = urldecode(basename($_SERVER['REQUEST_URI']));
-preg_match('#^(?P<name>[^!]+)!(?P<size>\d+)\.(?P<ext>png|jpg|gif)#i', $req, $matches);
+preg_match('#^(\w+)!(\d+)\.(png|jpg|gif)#i', $req, $matches);
 if (!$matches) {
     die();
 }
-$size = intval($matches['size']);
+[$whole, $name, $size, $ext] = $matches;
+$size = intval($size);
 if (($size > 2000) || ($size % 50 != 0))
     die();
-$f = $matches['name'] . '.' . $matches['ext'];
+$f = $name . '.' . $ext;
 list($width, $height) = getimagesize($f);
 $src = imagecreatefromstring(file_get_contents($f));
 $dst = imagescale($src, $size);
