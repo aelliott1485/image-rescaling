f6ce721800a47731cb70227b4ccdd6f1398ce8ac
Andrew Elliott <aelliott@onatlas.com>
2022-02-28 09:17:22 -0800

    simplify destructuring, add header and output image to browser

--- src/index.php
+++ src/index.php
@@ -1,11 +1,10 @@
 <?php
-//echo 'Hello world from my own Mac OS X server';
 $req = urldecode(basename($_SERVER['REQUEST_URI']));
 preg_match('#^(\w+)!(\d+)\.(png|jpg|gif)#i', $req, $matches);
 if (!$matches) {
     die();
 }
-[$whole, $name, $size, $ext] = $matches;
+[, $name, $size, $ext] = $matches;
 $size = intval($size);
 if (($size > 2000) || ($size % 50 != 0))
     die();
@@ -13,5 +12,6 @@ $f = $name . '.' . $ext;
 list($width, $height) = getimagesize($f);
 $src = imagecreatefromstring(file_get_contents($f));
 $dst = imagescale($src, $size);
+header("Content-Type: image/$ext");
 imagejpeg($dst, $req);
-?>
+imagejpeg($dst);
